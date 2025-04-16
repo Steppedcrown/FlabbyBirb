2d3a5016a08a6fe4124a56fe2edbfb5d44ba491a
Steppedcrown <steppedcrown@gmail.com>
2025-04-16 11:28:21 -0700

    Finished tutorial

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -3,15 +3,32 @@ using UnityEngine;
 public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
+    public float flapStength = 10;
+    public LogicScript logic;
+    public bool birdIsAlive = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        myRigidbody.linearVelocity = Vector2.up * 10;
+        if (gameObject.transform.position.y < -25)
+        {
+            logic.gameOver();
+            birdIsAlive = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && birdIsAlive)
+        {
+            myRigidbody.linearVelocity = Vector2.up * flapStength;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive = false;
     }
 }
